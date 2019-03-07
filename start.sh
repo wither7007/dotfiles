c8aaed43a898aa6aba0c302087525fdb3c1aa50b
Internet <Internet10@hclib.org>
2019-03-07 09:37:38 -0600

    added shell start

---
+++ start.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/wither7007/dotfiles/master/vim/.vimrc
+curl https://raw.githubusercontent.com/wither7007/dotfiles/master/vim/.vimrc > .vimrc
+curl https://raw.githubusercontent.com/wither7007/dotfiles/master/aliases/.bash_aliases > alias
+source alias
