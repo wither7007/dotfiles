4756ff4940a949cce4c8b90cd2477cd67df9c289
wither7007 <wither7007@gmail.com>
2019-05-01 10:22:21 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-curl https://raw.githubusercontent.com/wither7007/dotfiles/master/vim/.vimrc
 curl https://raw.githubusercontent.com/wither7007/dotfiles/master/vim/.vimrc > .vimrc
-curl https://raw.githubusercontent.com/wither7007/dotfiles/master/aliases/.bash_aliases > alias
-source alias
+curl https://raw.githubusercontent.com/wither7007/dotfiles/master/aliases/.bash_aliases > .bash_aliases	
+source .bash_aliases
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall"
