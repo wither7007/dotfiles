a5700bc1bed015d13c2df05686c90aaa96525796
Internet <Internet10@hclib.org>
2019-05-16 15:35:07 -0500

    prompt

--- start.sh
+++ start.sh
@@ -10,3 +10,7 @@ function fin()
 {
 history | grep $1 | sed 's/^\s*[0-9]*\s*//g' | sort | uniq |  vim -
 }
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
