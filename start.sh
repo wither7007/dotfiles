4816e85c5f0d13511ab1515656f461d78818161d
Internet <Internet10@hclib.org>
2019-05-16 13:10:08 -0500

    added fin

--- start.sh
+++ start.sh
@@ -4,3 +4,9 @@ curl https://raw.githubusercontent.com/wither7007/dotfiles/master/aliases/.bash_
 source .bash_aliases
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall"
+
+
+function fin()
+{
+history | grep $1 | sed 's/^\s*[0-9]*\s*//g' | sort | uniq |  vim -
+}
