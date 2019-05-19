4dd30cd995616949ad3c009da845e911c14be39c
Bill Withers <wither7007@gmail.com>
2019-05-19 15:14:49 -0500

    echo bashrc

--- start.sh
+++ start.sh
@@ -5,12 +5,16 @@ source .bash_aliases
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall"
 
+cat >> ~/.bashrc<<'endmsg'
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+		 
+}
 
 function fin()
 {
 history | grep $1 | sed 's/^\s*[0-9]*\s*//g' | sort | uniq |  vim -
 }
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
 export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+endmsg
