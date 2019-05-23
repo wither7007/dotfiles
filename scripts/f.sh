ddee15af6215ca632767e0e196499f44c44cb0b2
Bill Withers <wither7007@gmail.com>
2019-05-23 19:10:39 +0000

    f.sh

---
+++ scripts/f.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: f.sh
+# 
+#         USAGE: ./f.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/23/2019 17:43
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo enter name:
+read GHUSER
+echo $GHUSER
+curl -s "https://api.github.com/users/$GHUSER/repos?per_page=100" | grep -o 'git@[^"]*'
