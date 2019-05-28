8a90917349f2792001cd056dea178cb2c6d8af0e
Jim Steffes <jimsteffes@outlook.com>
2019-05-28 16:23:00 -0500

    k.sh

---
+++ k.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: k.sh
+# 
+#         USAGE: ./k.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/28/2019 16:18
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+git credential-manager delete https://github.com
+# git push https://wither7007:Jansen476@github.com/wither7007/dotfiles.git
+
