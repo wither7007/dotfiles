1d6075e0716aea22253ae0f2578c02d7ec0235c9
Bill Withers <wither7007@gmail.com>
2019-05-28 18:50:29 +0000

    pp.sh

---
+++ scripts/pp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: pp.sh
+# 
+#         USAGE: ./pp.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 05/25/2019 18:47
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo jim
+
