70684891457527bac71b8d68e8737bb6051c187a
Bill Withers <wither7007@gmail.com>
2019-07-05 20:07:38 +0000

    another

--- scripts/opts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while getopts ":a:" opt; do
-    case $opt in
-        a) echo "you passed the -a option with the $OPTARG argument" >&2 ;;
-        :) echo "Option -$opt requires an argument" >&2 ; exit 1 ;;
-        \?) echo "Invaled option: -$opt" >&2 ;;
-    esac
-done
--- scripts/pass.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-declare -a passarray
-mapfile passarray < "$1"
-echo ${passarray[6]}
